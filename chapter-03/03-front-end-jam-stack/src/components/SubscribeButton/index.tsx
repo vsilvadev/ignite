e3dc1f185f631612bc00f8502cc393862cc0df22
Vitor Silva <vitorabsilva10@gmail.com>
2021-03-30 18:55:03 -0300

    Finalization of the application using SSG for the preview pages of posts

---
+++ chapter-03/03-front-end-jam-stack/src/components/SubscribeButton/index.tsx
@@ -0,0 +1,49 @@
+import { useSession, signIn } from "next-auth/client";
+import { useRouter } from "next/dist/client/router";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
+import styles from "./styles.module.scss";
+
+interface SubscribeButtonProps {
+    priceId: string;
+}
+
+export function SubscribeButton ({priceId}: SubscribeButtonProps) {
+    const [session] = useSession();
+    const router = useRouter();
+    
+    async function handleSubscribe() {
+        if (!session)  {
+            signIn("github")
+            return;
+        }
+
+        if (session.activeSubscription) {
+            alert("Você já possui uma inscrição!");
+            router.push("/posts");
+            return;
+        }
+
+        try {
+            const response = await api.post("/subscribe")
+
+            const { sessionId } = response.data;
+
+            const stripe = await getStripeJs();
+
+            await stripe.redirectToCheckout({sessionId});
+        } catch (err) {
+            alert(err.message);
+        }
+    }
+
+    return (
+        <button 
+            type="button"
+            className={styles.subscribeButton}
+            onClick={handleSubscribe}
+        >
+            🚀 Subscribe Now!
+        </button>
+    );
+}
